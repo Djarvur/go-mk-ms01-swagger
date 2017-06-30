50978460ba9e3798a19aafc5d0a8915eb43b7f8e
Daniel Podolsky <onokonem@gmail.com>
2017-06-30 11:33:19 +0300

    master-class finished

---
+++ restapi/doc.go
@@ -0,0 +1,24 @@
+/*Package restapi testping
+
+This is a simple ping rest service
+
+
+    Schemes:
+      http
+    Host: localhost:8080
+    BasePath: /testping
+    Version: 0.0.1
+    License: MIT
+    Contact: <onokonem@gmail.com>
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+
+swagger:meta
+*/
+package restapi
